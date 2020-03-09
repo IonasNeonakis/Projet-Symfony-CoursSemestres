afa7caccb06e4f87302832b2b7186bc63aab20e2
ionasNeonakis <ionas.neonakis@gmail.com>
2020-03-09 11:53:47 +0100

    changement du css, ajout de modification et creation, il reste a faire le delete

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -6,7 +6,12 @@ namespace App\Controller;
 
 use App\Entity\Cours;
 use App\Entity\Semestre;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CoursController extends AbstractController
@@ -23,15 +28,30 @@ class CoursController extends AbstractController
 
     /**
      * @Route("/cours/creer", name="creer_cours")
+     * @Route("/cours/{id}/edit", name="edit_cours")
      */
-    public  function creerCours(Request $request){
-        $cours = new Cours();
+    public  function creerCours(Cours $cours = null ,Request $request, EntityManagerInterface $manager){
+
+        if(!$cours){
+            $cours = new Cours();
+        }
+
+        $se= new Semestre();
         $form = $this->createFormBuilder($cours)
-            ->add('Nom')
-            ->add('description')
-            ->add('numero Semestre')
+            ->add('nom')
+            ->add('description', TextareaType::class)
+            ->add('semestre', EntityType::class, [
+                'class'=>Semestre::class,
+                'choice_label'=> 'numeroSemestre'])
             ->getForm();
         $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $manager->persist($cours);
+            $manager->flush();
+
+            return $this->redirectToRoute('cour_show',['id'=>$cours->getId()]);
+        }
 
        return $this->render("creercours.html.twig", ['formCours'=> $form->createView()]);
     }
--- src/Controller/SemestreController.php
+++ src/Controller/SemestreController.php
@@ -6,7 +6,10 @@ namespace App\Controller;
 
 use App\Entity\Cours;
 use App\Entity\Semestre;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -15,10 +18,49 @@ class SemestreController extends AbstractController
     /**
      * @Route("/Semestre",name="accueilSemestres")
      */
-    public function accueilSemestres(){
+    public function accueilSemestres()
+    {
         $repo = $this->getDoctrine()->getRepository(Semestre::class);
-        $semestres=$repo->findAll();
-        return $this->render('accueuilsemestres.html.twig',['semestres'=>$semestres]);
+        $semestres = $repo->findAll();
+        return $this->render('accueuilsemestres.html.twig', ['semestres' => $semestres]);
+    }
+
+    /**
+     * @Route("Semestre/creer_semestre", name="creer_semestre")
+     * @Route("Semestre/{id}/edit_semestre", name="edit_semestre")
+     */
+    public function creerSemestre(Semestre $semestre = null, Request $request, EntityManagerInterface $manager)
+    {
+
+        if (!$semestre) {
+            $semestre = new Semestre();
+        }
+
+        $se = new Semestre();
+        $form = $this->createFormBuilder($semestre)
+            ->add('numeroSemestre')
+            ->add('nomFormation')
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($semestre);
+            $manager->flush();
+
+            return $this->redirectToRoute('semestre_show', ['id' => $semestre->getId()]);
+        }
+
+        return $this->render("creersemestre.html.twig", ['formSemestre' => $form->createView()]);
+    }
+
+    /**
+     * @Route("Semestre/{id}", name="semestre_show")
+     */
+    public function show($id){
+        $repo=$this->getDoctrine()->getRepository(Semestre::class);
+        $semestre = $repo->find($id);
+
+        return $this->render('semestre.html.twig' , ['semestre' => $semestre]);
     }
 
 }
